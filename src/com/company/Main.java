420602e9b2e1802bef929e33f493805648df2124
kaimxan <kaimxan.01@gmail.com>
2022-03-12 21:17:47 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+    User user= new User();
+    user.setName("nurbek");
+    user.setAge(20);
+    user.setDg("fgdfg");
+        System.out.println(user.getAge()+user.getDg()+user.getName());
+
+
+
+
+
+    }
+}
---
+++ src/com/company/User.java
@@ -0,0 +1,31 @@
+package com.company;
+
+public class User {
+    private String name;
+    private int age;
+    private String dg;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDg() {
+        return dg;
+    }
+
+    public void setDg(String dg) {
+        this.dg = dg;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
